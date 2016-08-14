843f920cca410c63005a2c678c2084ceb99cb8ca
Isaac Elliott <isaace71295@gmail.com>
2016-08-15 00:08:23 +1000

    Start work

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,37 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Halogen
+import Halogen.HTML.Indexed as H
+import Halogen.HTML.Events.Indexed as E
+import Halogen.HTML.Properties.Indexed
+import Halogen.Util (awaitBody, runHalogenAff)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+apiURL :: String
+apiURL = "https://127.0.0.1:3000"
+
+data LoginQuery a = LoginQuery a
+
+loginForm :: forall g. Component { loggedIn :: Boolean } LoginQuery g
+loginForm = component { render, eval }
+  where
+    render state
+      | not state.loggedIn = H.div_ [
+            H.form_ [
+                H.input [inputType InputText, name "username"] 
+                , H.input [inputType InputPassword, name "password"] 
+                , H.input [inputType InputSubmit, value "Log in"] 
+            ]
+        ]
+      | otherwise = H.div_ [
+            H.button [value "Log out"] [] 
+        ]
+    
+    eval :: LoginQuery ~> ComponentDSL { loggedIn :: Boolean } LoginQuery g
+    eval (LoginQuery a) = pure a
+
+main :: forall e. Eff (HalogenEffects ()) Unit
+main = runHalogenAff do
+    body <- awaitBody
+    driver <- runUI loginForm { loggedIn: false } body
+    pure unit
