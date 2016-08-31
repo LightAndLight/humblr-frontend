8ec8d616ae2546b748f7d67f4d63bbe70193769c
Isaac Elliott <isaace71295@gmail.com>
2016-08-31 22:23:10 +1000

    Added Post and PostList components

---
+++ src/Humblr/Components/Post.purs
@@ -0,0 +1,106 @@
+module Humblr.Components.Post (
+    PostState
+    , PostQuery(..)
+    , initialPostState
+    , postComponent
+) where
+
+import Prelude
+import Halogen
+import Halogen.HTML.Indexed as H
+import Halogen.HTML.Properties.Indexed as HP
+import Halogen.HTML.Events.Indexed as E
+
+import Humblr.Config
+
+type PostState = {
+    id :: Int
+    , title :: String
+    , body :: String
+    , author :: String
+    , editable :: Boolean
+    , editing :: Boolean
+    , markedForDelete :: Boolean
+    }
+
+initialPostState :: Int -> String -> String -> String -> PostState
+initialPostState pid title body author = {
+    id: pid
+    , title: title
+    , body: body
+    , author: author
+    , editable: false
+    , editing: false
+    , markedForDelete: false
+    }
+
+data PostQuery a = Edit a
+                 | Save a
+                 | SetBody String a
+                 | SetTitle String a
+                 | MarkDelete a
+                 | UnmarkDelete a
+                 | Delete a
+
+postComponent :: forall e. Component PostState PostQuery e
+postComponent = component { render, eval }
+  where
+    renderDeleteConfirm :: PostState -> ComponentHTML PostQuery
+    renderDeleteConfirm state
+      | state.markedForDelete = H.div_ [
+            H.p_ [H.text "Confirm delete"]
+            , H.button [HP.value "Yes", E.onClick (E.input_ Delete)] []
+            , H.button [HP.value "No", E.onClick (E.input_ UnmarkDelete)] []
+            ]
+      | otherwise = H.button [HP.value "Delete", E.onClick (E.input_ MarkDelete)] []
+
+    renderUpdatePost :: PostState -> Array (ComponentHTML PostQuery)
+    renderUpdatePost state
+      | state.editable = [
+            H.button [HP.value "Edit", E.onClick (E.input_ Edit)] []
+            , renderDeleteConfirm state
+            ]
+      | otherwise = []
+
+    render :: PostState -> ComponentHTML PostQuery
+    render state
+      | state.editing = H.div_ [
+            H.input [HP.inputType HP.InputText, HP.value state.title, E.onValueChange (E.input SetTitle)]
+            , H.textarea [HP.value state.body, E.onValueChange (E.input SetBody)]
+            , H.button [HP.value "Save", E.onClick (E.input_ Save)] []
+            ]
+      | otherwise = H.div_ $ [
+            H.h1_ [H.text state.title]
+            , H.p_ [H.text state.author]
+            , H.p_ [H.text state.body]
+            ] <> renderUpdatePost state
+
+    eval :: forall g. PostQuery ~> ComponentDSL PostState PostQuery g
+    eval (Edit next) = do
+        editable <- gets _.editable
+        when editable $ modify (_ { editing = true })
+        pure next
+    eval (Save next) = do
+        editable <- gets _.editable
+        when editable do
+            -- send to db
+            modify (_ { editing = false })
+        pure next
+    eval (SetTitle title next) = do
+        modify (_ { title = title })
+        pure next
+    eval (SetBody body next) = do
+        modify (_ { body = body })
+        pure next
+    eval (MarkDelete next) = do
+        modify (_ { markedForDelete = true })
+        pure next
+    eval (UnmarkDelete next) = do
+        modify (_ { markedForDelete = false })
+        pure next
+    eval (Delete next) = do
+        marked <- gets _.markedForDelete
+        when marked do
+            -- delete from DB
+            pure unit
+        pure next
---
+++ src/Humblr/Components/PostList.purs
@@ -0,0 +1,71 @@
+module Humblr.Components.PostList (
+    Post
+    , PostListState
+    , PostListQuery
+    , PostSlot
+    , initialPostListState
+    , postListComponent
+) where
+
+import Prelude
+import Data.Array (filter)
+import Data.Functor.Coproduct (Coproduct)
+import Data.Generic (gEq, gCompare)
+import Data.Maybe (Maybe(..))
+import Halogen
+import Halogen.HTML.Indexed as H
+import Halogen.HTML.Properties.Indexed as HP
+import Halogen.HTML.Events.Indexed as E
+
+import Humblr.Components.Post
+
+type Post = { id :: Int, title :: String, body :: String, author :: String }
+
+type PostListState = { posts :: Array Post }
+
+initialPostListState :: PostListState
+initialPostListState = { posts: [] }
+
+data PostListQuery a = Load (Array Post) a
+
+newtype PostSlot = PostSlot Int
+derive instance eqPostSlot :: Eq PostSlot
+derive instance ordPostSlot :: Ord PostSlot
+
+type State g = ParentState PostListState PostState PostListQuery PostQuery g PostSlot
+type Query = Coproduct PostListQuery (ChildF PostSlot PostQuery)
+
+postListComponent :: forall g. Functor g => Component (State g) Query g
+postListComponent = parentComponent $ { render: render, eval: eval, peek: Just peek }
+  where
+    renderPost :: forall f.
+                  Post
+               -> ParentHTML PostState PostListQuery PostQuery g PostSlot
+    renderPost post = H.slot (PostSlot post.id) \_ -> {
+        component: postComponent
+        , initialState: initialPostState post.id post.title post.body post.author
+        }
+    
+    render :: forall f.
+              PostListState
+           -> ParentHTML PostState PostListQuery PostQuery g PostSlot
+    render state = H.div_ $ map renderPost state.posts
+
+    eval :: forall f.
+            PostListQuery
+         ~> ParentDSL PostListState PostState PostListQuery PostQuery g PostSlot
+    eval (Load posts next) = do
+        modify (_ { posts = posts }) 
+        pure next
+
+    removePost :: PostSlot -> PostListState -> PostListState
+    removePost (PostSlot pid) state = state { posts = filter (\p -> p.id /= pid) state.posts }
+
+    peek :: forall f x.
+            ChildF PostSlot PostQuery x
+         -> ParentDSL PostListState PostState PostListQuery PostQuery g PostSlot Unit
+    peek (ChildF slot query) = case query of
+        Delete _ -> do
+            posts <- gets _.posts
+            modify $ removePost slot
+        _        -> pure unit
