fd389071b97a8a01b1d52b7d2090a9a83f653439
Isaac Elliott <isaace71295@gmail.com>
2016-09-04 15:43:31 +1000

    Delete post now integrates with the backend

--- src/Humblr/Components/Dashboard.purs
+++ src/Humblr/Components/Dashboard.purs
@@ -6,24 +6,25 @@ module Humblr.Components.Dashboard (
 ) where
 
 import Prelude
+import Halogen
+import Humblr.Components.Login
+import Humblr.Components.Post
+import Humblr.Components.PostList
+import Halogen.HTML.Indexed as H
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
+import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode ((.?), class DecodeJson, decodeJson)
 import Data.Array ((:))
 import Data.Either (Either(..))
-import Data.Functor.Coproduct (Coproduct(..))
+import Data.Functor.Coproduct (Coproduct(..), coproduct)
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Generic (gEq, gCompare)
-import Halogen
 import Halogen.Component.ChildPath (ChildPath, cpL, cpR, (:>))
-import Halogen.HTML.Indexed as H
-import Network.HTTP.Affjax (AJAX)
+import Humblr.Config (AppEffects, authCookieName, apiURL)
+import Humblr.Requests (getWithToken, deleteWithToken)
+import Network.HTTP.Affjax (AffjaxResponse, AJAX)
+import Network.HTTP.StatusCode (StatusCode(..))
 import Web.Storage (STORAGE, getItem, session)
-
-import Humblr.Components.Login
-import Humblr.Components.PostList
-import Humblr.Config
-import Humblr.Requests
 
 type DashboardState = {
     token :: Maybe String
@@ -36,14 +37,14 @@ initialDashboardState = { token: Nothing, username: Nothing, err: Nothing }
 
 data DashboardQuery a = Posts a
 
-type ChildState e = Either LoginState (PostListState (Aff (AppEffects e)))
+type ChildState e = Either LoginState (PostListState e)
 type ChildQuery = Coproduct LoginQuery PostListQuery
 type ChildSlot = Either Unit Unit
 
 cpLogin :: forall e. ChildPath LoginState (ChildState e) LoginQuery ChildQuery Unit ChildSlot
 cpLogin = cpL
 
-cpPostList :: forall e. ChildPath (PostListState (Aff (AppEffects e))) (ChildState e) PostListQuery ChildQuery Unit ChildSlot
+cpPostList :: forall e. ChildPath (PostListState e) (ChildState e) PostListQuery ChildQuery Unit ChildSlot
 cpPostList = cpR
 
 type State e = ParentState DashboardState (ChildState e) DashboardQuery ChildQuery (Aff (AppEffects e)) ChildSlot
@@ -77,40 +78,62 @@ dashboardComponent = parentComponent $ { render: render, eval: eval, peek: Just 
          ~> ParentDSL DashboardState (ChildState f) DashboardQuery ChildQuery (Aff (AppEffects f)) ChildSlot
     eval (Posts next) = pure next
 
+    peekLogin :: forall f a.
+                 LoginQuery a
+              -> ParentDSL DashboardState (ChildState f) DashboardQuery ChildQuery (Aff (AppEffects f)) ChildSlot Unit
+    peekLogin (Login _ _ _) = do
+        isLoggedIn <- query' cpLogin unit $ request IsLoggedIn
+        when (fromMaybe false isLoggedIn) do
+            state <- get 
+            case state.token of
+                Nothing -> do
+                    token' <- fromEff getToken
+                    modify (_ { token = token' })
+                Just token' -> pure unit
+
+            state' <- get
+            case state'.token of
+                Nothing -> modify (_ { err = Just "Token disappeared" })
+                Just token' -> do
+                    res <- fromAff $ getMe token'
+                    case res of
+                        Right (UserProfile me) -> modify (_ { username = Just me.username })
+                        Left err -> modify (_ { err = Just err })
+                    res' <- fromAff $ getPosts token'
+                    username <- gets _.username
+                    case res' of
+                        Right posts -> do
+                            query' cpPostList unit $ action (Coproduct <<< Left <<< Load username posts)
+                            pure unit
+                        Left err -> modify (_ { err = Just err })
+    peekLogin (Logout _) = do
+        query' cpPostList unit $ action (Coproduct <<< Left <<< Clear)
+        modify \_ -> initialDashboardState
+    peekLogin _ = pure unit
+
+    peekPost :: forall f x.
+                ChildF PostSlot PostQuery x
+             -> ParentDSL DashboardState (ChildState f) DashboardQuery ChildQuery (Aff (AppEffects f)) ChildSlot Unit
+    peekPost (ChildF slot q) = case q of
+        Delete _ -> do
+            maybeToken <- gets _.token
+            response <- fromAff $ deletePost maybeToken slot
+            if (response.status == StatusCode 200)
+                then do
+                    query' cpPostList unit (action (Coproduct <<< Left <<< RemovePost slot))
+                    pure unit
+                else pure unit
+        _ -> pure unit
+
+    peekPostList :: forall x f.
+                    PostListQuery x
+                 -> ParentDSL DashboardState (ChildState f) DashboardQuery ChildQuery (Aff (AppEffects f)) ChildSlot Unit
+    peekPostList = coproduct (const $ pure unit) peekPost
+
     peek :: forall x f.
             ChildF ChildSlot ChildQuery x
          -> ParentDSL DashboardState (ChildState f) DashboardQuery ChildQuery (Aff (AppEffects f)) ChildSlot Unit
-    peek (ChildF (Left unit) (Coproduct (Left q))) = case q of
-        Login _ _ _ -> do
-            isLoggedIn <- query' cpLogin unit $ request IsLoggedIn
-            when (fromMaybe false isLoggedIn) do
-                state <- get 
-                case state.token of
-                    Nothing -> do
-                        token' <- fromEff getToken
-                        modify (_ { token = token' })
-                    Just token' -> pure unit
-
-                state <- get
-                case state.token of
-                    Nothing -> modify (_ { err = Just "Token disappeared" })
-                    Just token' -> do
-                        res <- fromAff $ getMe token'
-                        case res of
-                            Right (UserProfile me) -> modify (_ { username = Just me.username })
-                            Left err -> modify (_ { err = Just err })
-                        res <- fromAff $ getPosts token'
-                        username <- gets _.username
-                        case res of
-                            Right posts -> do
-                                query' cpPostList unit $ action (Coproduct <<< Left <<< Load username posts)
-                                pure unit
-                            Left err -> modify (_ { err = Just err })
-        Logout _ -> do
-            query' cpPostList unit $ action (Coproduct <<< Left <<< Clear)
-            modify \_ -> initialDashboardState
-        _ -> pure unit
-    peek _ = pure unit
+    peek (ChildF _ q) = coproduct peekLogin peekPostList q
 
 getToken :: forall e. Eff (storage :: STORAGE | e) (Maybe String)
 getToken = do
@@ -134,3 +157,10 @@ getPosts :: forall e. String -> Aff (ajax :: AJAX | e) (Either String (Array Pos
 getPosts token = do
     res <- getWithToken token "/my/posts"
     pure $ decodeJson res.response
+
+deletePost :: forall e.
+              Maybe String
+           -> PostSlot
+           -> Aff (ajax :: AJAX | e) (AffjaxResponse Json)
+deletePost token (PostSlot pid) =
+  deleteWithToken (fromMaybe "" token) (apiURL <> "/post/" <> show pid <> "/delete")
--- src/Humblr/Components/PostList.purs
+++ src/Humblr/Components/PostList.purs
@@ -4,23 +4,23 @@ module Humblr.Components.PostList (
     , PostListQuery
     , PostListState'
     , PostListQuery'(..)
-    , PostSlot
+    , PostSlot(..)
     , initialPostListState
     , postListComponent
 ) where
 
-import Prelude
+import Prelude (class Ord, class Eq, type (~>), pure, bind, map, ($), (/=), (==))
+import Control.Monad.Aff (Aff)
 import Data.Argonaut.Decode ((.?), class DecodeJson, decodeJson)
 import Data.Array (filter)
 import Data.Functor.Coproduct (Coproduct)
-import Data.Generic (gEq, gCompare)
-import Data.Maybe (Maybe(..))
-import Halogen
+import Data.Maybe (Maybe(Nothing, Just))
+import Halogen (ParentDSL, ParentHTML, Component, ChildF, ParentState, parentComponent, modify, set)
 import Halogen.HTML.Indexed as H
-import Halogen.HTML.Properties.Indexed as HP
-import Halogen.HTML.Events.Indexed as E
 
-import Humblr.Components.Post
+import Humblr.Components.Post (PostQuery, PostState, initialPostState, postComponent)
+import Humblr.Config (AppEffects)
+
 
 newtype Post = Post { id :: Int, title :: String, body :: String, author :: String }
 
@@ -44,17 +44,18 @@ initialPostListState :: PostListState'
 initialPostListState = { username: Nothing, posts: [] }
 
 data PostListQuery' a = Load (Maybe String) (Array Post) a
+                      | RemovePost PostSlot a
                       | Clear a
 
 newtype PostSlot = PostSlot Int
 derive instance eqPostSlot :: Eq PostSlot
 derive instance ordPostSlot :: Ord PostSlot
 
-type PostListState g = ParentState PostListState' PostState PostListQuery' PostQuery g PostSlot
+type PostListState e = ParentState PostListState' PostState PostListQuery' PostQuery (Aff (AppEffects e)) PostSlot
 type PostListQuery = Coproduct PostListQuery' (ChildF PostSlot PostQuery)
 
-postListComponent :: forall g. Functor g => Component (PostListState g) PostListQuery g
-postListComponent = parentComponent $ { render: render, eval: eval, peek: Just peek }
+postListComponent :: forall e. Component (PostListState e) PostListQuery (Aff (AppEffects e))
+postListComponent = parentComponent $ { render: render, eval: eval, peek: Nothing }
   where
     isAuthor :: Maybe String -> String -> Boolean
     isAuthor Nothing _ = false
@@ -63,7 +64,7 @@ postListComponent = parentComponent $ { render: render, eval: eval, peek: Just p
     renderPost :: forall f.
                   Maybe String
                -> Post
-               -> ParentHTML PostState PostListQuery' PostQuery g PostSlot
+               -> ParentHTML PostState PostListQuery' PostQuery (Aff (AppEffects f)) PostSlot
     renderPost username (Post post) = H.slot (PostSlot post.id) \_ -> {
         component: postComponent
         , initialState: initialPostState post.id post.title post.body post.author (isAuthor username post.author)
@@ -71,27 +72,21 @@ postListComponent = parentComponent $ { render: render, eval: eval, peek: Just p
     
     render :: forall f.
               PostListState'
-           -> ParentHTML PostState PostListQuery' PostQuery g PostSlot
+           -> ParentHTML PostState PostListQuery' PostQuery (Aff (AppEffects f)) PostSlot
     render state = H.div_ $ map (renderPost state.username) state.posts
+
+    removePost :: PostSlot -> PostListState' -> PostListState'
+    removePost (PostSlot pid) state = state { posts = filter (\(Post p) -> p.id /= pid) state.posts }
 
     eval :: forall f.
             PostListQuery'
-         ~> ParentDSL PostListState' PostState PostListQuery' PostQuery g PostSlot
+         ~> ParentDSL PostListState' PostState PostListQuery' PostQuery (Aff (AppEffects f)) PostSlot
     eval (Load username posts next) = do
         modify (_ { username = username, posts = posts }) 
         pure next
     eval (Clear next) = do
         set initialPostListState
         pure next
-
-    removePost :: PostSlot -> PostListState' -> PostListState'
-    removePost (PostSlot pid) state = state { posts = filter (\(Post p) -> p.id /= pid) state.posts }
-
-    peek :: forall f x.
-            ChildF PostSlot PostQuery x
-         -> ParentDSL PostListState' PostState PostListQuery' PostQuery g PostSlot Unit
-    peek (ChildF slot query) = case query of
-        Delete _ -> do
-            posts <- gets _.posts
-            modify $ removePost slot
-        _        -> pure unit
+    eval (RemovePost slot next) = do
+        modify $ removePost slot
+        pure next
