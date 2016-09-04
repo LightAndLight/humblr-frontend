e05c9dd90b9bdeaa26eabcfe9d669aedcf900d5c
Isaac Elliott <isaace71295@gmail.com>
2016-09-04 15:40:53 +1000

    Explicit imports

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,12 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, unit, pure, bind)
 import Control.Monad.Eff (Eff)
 import Halogen (parentState, runUI)
 import Halogen.Util (awaitBody, runHalogenAff)
 
-import Humblr.Components.Dashboard
-import Humblr.Config
+import Humblr.Components.Dashboard (initialDashboardState, dashboardComponent)
+import Humblr.Config (AppEffects)
 
 main :: Eff (AppEffects ()) Unit
 main = runHalogenAff do
