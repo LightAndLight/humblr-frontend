90af331d0dfae5bf5c8f345905ad53fae67429a2
Isaac Elliott <isaace71295@gmail.com>
2016-09-04 22:02:06 +1000

    Formatted Requests

--- src/Humblr/Requests.purs
+++ src/Humblr/Requests.purs
@@ -16,31 +16,47 @@ import Network.HTTP.Affjax.Response (class Respondable)
 import Network.HTTP.RequestHeader (RequestHeader(..))
 
 getWithToken :: forall e a. Respondable a => String -> URL -> Affjax e a
-getWithToken token url = affjax defaultRequest {
-  method = Left GET
-  , headers = RequestHeader "auth" token : defaultRequest.headers
-  , url = url
-  }
+getWithToken token url
+  = affjax defaultRequest
+      { method = Left GET
+      , headers = RequestHeader "auth" token : defaultRequest.headers
+      , url = url
+      }
 
 deleteWithToken :: forall e a. Respondable a => String -> URL -> Affjax e a
-deleteWithToken token url = affjax defaultRequest {
-  method = Left DELETE
-  , headers = RequestHeader "auth" token : defaultRequest.headers
-  , url = url
-  }
+deleteWithToken token url
+  = affjax defaultRequest
+      { method = Left DELETE
+      , headers = RequestHeader "auth" token : defaultRequest.headers
+      , url = url
+      }
 
-postWithToken :: forall e a b. (Requestable a, Respondable b) => String -> URL -> a -> Affjax e b
-postWithToken token url content = affjax defaultRequest {
-  content = Just content
-  , method = Left POST
-  , headers = RequestHeader "auth" token : defaultRequest.headers
-  , url = url
-  }
+postWithToken :: forall e a b.
+                (Requestable a, Respondable b) 
+              => String
+              -> URL
+              -> a
+              -> Affjax e b
 
-patchWithToken :: forall e a b. (Requestable a, Respondable b) => String -> URL -> a -> Affjax e b
-patchWithToken token url content = affjax defaultRequest {
-  content = Just content
-  , method = Left PATCH
-  , headers = RequestHeader "auth" token : defaultRequest.headers
-  , url = url
-  }
+postWithToken token url content
+  = affjax defaultRequest
+      { content = Just content
+      , method = Left POST
+      , headers = RequestHeader "auth" token : defaultRequest.headers
+      , url = url
+      }
+
+patchWithToken :: forall e a b.
+                 (Requestable a, Respondable b)
+               => String
+               -> URL
+               -> a
+               -> Affjax e b
+
+patchWithToken token url content
+  = affjax defaultRequest
+      { content = Just content
+      , method = Left PATCH
+      , headers = RequestHeader "auth" token : defaultRequest.headers
+      , url = url
+      }
